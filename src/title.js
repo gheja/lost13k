c9f3abbbb28213ae51998a36c41f62a8406c9962
Gabor Heja <gheja@m002560p>
2017-09-12 23:33:24 +0200

    title: added basic intro

--- src/title.js
+++ src/title.js
@@ -17,26 +17,115 @@ function titleSkip()
 
 function drawTitle()
 {
-	// ctx.globalCompositeOperation = "source-over";
-	// ctx.fillStyle = "#000";
+	let i, a, s;
 	
-	ctx.fillRect(0, 0, _windowWidth, _windowHeight);
-	
-	ctx.fillStyle = "#fff";
-	ctx.font = _scale(15) + "px Arial";
-	/*
-	ctx.fillText("This is the Lorem Ipsum freighter");
-	ctx.fillText("shipping goods from planets to planets");
-	ctx.fillText("And moons, occasionally.");
-	"This is the captain, Jeff"
-	"and his four cats as companions for the rides"
-	"Bubble"
-	"Bobby"
-	"Marshmallow"
-	"and Winston"
-	"and Winston?"
-	"Where's Winston?"
-	*/
+	if (_gameState == GAME_STATE_READY)
+	{
+		a = [];
+		
+		for (i=0; i<_cats.length; i++)
+		{
+			if (_cats[i].location != null)
+			{
+				a.push(_cats[i].name);
+			}
+		}
+		
+		if (a.length > 1)
+		{
+			s = "Where are " + a.join(", ") + "?";
+		}
+		else
+		{
+			s = "Where is " + a + "?";
+		}
+		
+		// ctx.fillStyle = "#000";
+		ctx.fillRect(0, 0, _windowWidth, _windowHeight);
+		
+		ctx.fillStyle = "#fff";
+		ctx.font = _scale(15) + "px Arial";
+		ctx.textAlign = "center";
+		
+		if (_sceneTime > 0.5)
+		{
+			ctx.fillText("This is the xxxxxxxxxx freighter", _x(0), _y(-160));
+		}
+		
+		if (_sceneTime > 1.5)
+		{
+			ctx.fillText("and its captain, Jeff.", _x(0), _y(-90));
+		}
+		
+		if (_sceneTime > 2.0)
+		{
+			ctx.fillText("He is shipping goods from planet to planet", _x(0), _y(-70));
+		}
+		
+		if (_sceneTime > 2.5)
+		{
+			ctx.fillText("with his four companions.", _x(0), _y(-50));
+		}
+		
+		if (_sceneTime > 3)
+		{
+			ctx.fillText(_cats[0].name + ((_cats[0].location != null && _sceneTime > 3.5) ? "?!" : ""), _x(-100), _y(20));
+		}
+		
+		if (_sceneTime > 4)
+		{
+			ctx.fillText(_cats[1].name + ((_cats[1].location != null && _sceneTime > 4.5) ? "?!" : ""), _x(100), _y(20));
+		}
+		
+		if (_sceneTime > 5)
+		{
+			ctx.fillText(_cats[2].name + ((_cats[2].location != null && _sceneTime > 5.5) ? "?!" : ""), _x(-100), _y(100));
+		}
+		
+		if (_sceneTime > 6)
+		{
+			ctx.fillText("and " + _cats[3].name + ((_cats[3].location != null && _sceneTime > 6.5) ? "?!" : ""), _x(100), _y(100));
+		}
+		
+		if (_sceneTime > 7)
+		{
+			ctx.fillText(s, _x(0), _y(130));
+		}
+		
+		function localDrawCat(cat, p)
+		{
+			drawMultipleShape([
+				[ SHAPE_CAT_HEAD_AWAKE, { x: 50, y: -40 }, 10, cat.colors[0], cat.colors[1] ],
+				[ SHAPE_CAT_EYE, { x: 75, y: -65 }, 10, null, cat.colors[2] ],
+				[ SHAPE_CAT_EYE, { x: 115, y: -65 }, 10, null, cat.colors[2] ]
+			], p, 0.4);
+		}
+		
+		if (_sceneTime > 1.0)
+		{
+			drawShip({ x: 0, y: -130 }, 2);
+		}
+		
+		if (_sceneTime > 3.5 && _cats[0].location == null)
+		{
+			localDrawCat(_cats[0], { x: -139, y: 10 });
+		}
+		
+		if (_sceneTime > 4.5 && _cats[1].location == null)
+		{
+			localDrawCat(_cats[1], { x: 61, y: 10 });
+		}
+		
+		if (_sceneTime > 5.5 && _cats[2].location == null)
+		{
+			localDrawCat(_cats[2], { x: -139, y: 90 });
+		}
+		
+		if (_sceneTime > 6.5 && _cats[3].location == null)
+		{
+			localDrawCat(_cats[3], { x: 61, y: 90 });
+		}
+	}
 	
 	switch (_gameState)
 	{
